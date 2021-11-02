a0c0d118c2e89e59d74ea8ebf68c2a8cd7ab3595
Kiet Do <kietdo0602@gmail.com>
2021-11-02 11:36:03 +0700

    Finish Menu / Start page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Button from '@mui/material/Button';
+import Send from '@mui/icons-material/Send';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="firstPage">
+      <div className="App">
+        <h1 className="firstPageTitle"> Gap Analysis Questionnaire </h1>
+        <h1 className="firstPageTitle"> Access Control (AC) </h1>
+      </div>
+      <div className="button-container">
+        <Button variant="contained" endIcon={<Send />}> Send </Button>
+      </div>
     </div>
   );
 }
